8c0f9d61fca2e2dead691b53cb51894d381bbbfd
Siang Meng <siang-meng@live.com>
2025-07-13 09:48:56 +0800

    allow running of the video agent

--- src/video_agent/main.py
+++ src/video_agent/main.py
@@ -1,5 +1,6 @@
 from agents import Agent, Runner
 from .tools import download_video, check_video_saved
+from ..models import VideoInfo
 
 video_agent = Agent(
     name="Video Agent",
@@ -25,5 +26,20 @@ video_agent = Agent(
 print()
 
 if __name__ == "__main__":
-    command = "download from this youtube link: https://www.youtube.com/watch?v=MG6tuszAIT8"
-    Runner.run_sync(video_agent, command)
+    while True:
+        command = input("What do you want? ")
+        res = Runner.run_sync(video_agent, command)
+        res_contents = res.final_output
+
+        # format output
+        if isinstance(res_contents, bool):
+            print(f"Video {'exists' if res_contents else 'does not exist'}")
+        elif isinstance(res_contents, list) and isinstance(res[0], VideoInfo):
+            print(f"Contents saved here: video_id = {res_contents[1]}")
+        elif res_contents is None or res_contents == [None, None]:
+            print(f"Could not process video, please try again later")
+        else:
+            print(res_contents)
+        continue_running = input("Continue?(Y to continue, anything else to stop) ").lower() == "y"
+        if not continue_running:
+            break
