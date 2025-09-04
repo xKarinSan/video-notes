c133223eefc8dfbdd8b43fe7269755a73d7d7b9d
Siang Meng <siang-meng@live.com>
2025-09-04 23:14:56 +0800

    fix vite entry

--- vnotes/forge.config.js
+++ vnotes/forge.config.js
@@ -2,19 +2,6 @@ const { FusesPlugin } = require("@electron-forge/plugin-fuses");
 const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 
 module.exports = {
-    publishers: [
-        {
-            name: "@electron-forge/publisher-github",
-            config: {
-                repository: {
-                    owner: "xKarinSan",
-                    name: "video-notes",
-                },
-                prerelease: false,
-                draft: true,
-            },
-        },
-    ],
     packagerConfig: {
         asar: true,
     },
@@ -39,10 +26,31 @@ module.exports = {
     ],
     plugins: [
         {
-            name: "@electron-forge/plugin-auto-unpack-natives",
-            config: {},
+            name: "@electron-forge/plugin-vite",
+            config: {
+                // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
+                // If you are familiar with Vite configuration, it will look really familiar.
+                build: [
+                    {
+                        // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
+                        entry: "src/main.js",
+                        config: "vite.main.config.mjs",
+                        target: "main",
+                    },
+                    {
+                        entry: "src/preload.js",
+                        config: "vite.preload.config.mjs",
+                        target: "preload",
+                    },
+                ],
+                renderer: [
+                    {
+                        name: "main_window",
+                        config: "vite.renderer.config.mjs",
+                    },
+                ],
+            },
         },
-
         // Fuses are used to enable/disable various Electron functionality
         // at package time, before code signing the application
         new FusesPlugin({
